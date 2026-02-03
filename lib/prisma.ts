fb85908e29a9ed21b195cf51f5d504400a0612a8
SoftWing Developer <deploy@softwing.cr>
2026-02-03 00:21:41 -0600

    Fix: Make DATABASE_URL explicit in schema and clean up code

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
-const DB_URL = "mysql://mariadb:2Pgt1yHb4zxjjB2KMzhbtllZpnn6FD1ddNdE3VJ4KvvHamwtsYmhZqYPA9mdtLdl@s4c4s0kg00cscg0ks8k8go8o:3306/ticketsystem"
-
 const prismaClientSingleton = () => {
-    return new PrismaClient({
-        datasources: {
-            db: {
-                url: DB_URL
-            }
-        }
-    })
+    return new PrismaClient()
 }
 
 declare global {
