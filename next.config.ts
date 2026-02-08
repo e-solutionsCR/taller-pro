36a9191539f2700ed65af946838018d99464ae93
SoftWing Developer <deploy@softwing.cr>
2026-02-07 18:48:35 -0600

    fix: Build errors (next.config experimental, import path)

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    // @ts-ignore
-    allowedDevOrigins: ['217.216.89.248:3000', 'localhost:3000', '217.216.89.248']
-  }
 };
 
 export default nextConfig;
